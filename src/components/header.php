8725e177d7d6a9b7e47eae9dfafc5aa683b8f948
leandrosantino <leandrosantino2013@gmail.com>
2023-09-23 21:24:43 -0300

    changed the colors palette and created the login page

---
+++ src/components/header.php
@@ -0,0 +1,19 @@
+<?php 
+    return '
+        <header>
+            <div>
+            <img id="logo-white" src="./assets/logo-white.png" alt="logo-white">
+            </div>
+            <div>
+            <nav>
+                <a href="/">Home</a>
+                <a href="">Conteúdos</a>
+                <a href="">Temas</a>
+            </nav>
+            </div>
+            <div>
+            <a id="login_link" href="/pages/login" >Entrar</a>
+            </div>
+        </header>
+    '
+?>
---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php 
+  $header = include './components/header.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/global.css">
+  <title>Home</title>
+</head>
+
+<body>
+  <?=$header?>
+
+  <main id="main_home">
+    <!-- Colocar o conteúdo da página aqui -->
+  </main>
+
+</body>
+
+</html>
