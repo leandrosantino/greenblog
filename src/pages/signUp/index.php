18c565bbd70fd28a469513c81aefb3bdef78bd9c
leandrosantino <leandrosantino2013@gmail.com>
2023-10-08 13:09:29 -0300

    update

---
+++ src/pages/signUp/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../styles/global.css">
+  <link rel="stylesheet" href="../../styles/login.css">
+  <title>Home</title>
+</head>
+
+<body>
+    <section>
+        <span>
+            Um lugar mais verde na Internet
+        </span>
+        <div>
+            <img src="../../assets/tree.png" alt="tree">
+        </div>
+    </section>
+    <section>
+        <img id="logo_green" src="../../assets/logo-green.png" alt="tree">
+        
+
+        <form id="login_form" action="/">
+            <h1>Login</h1>
+            <div>
+                <label for="">Email:</label>
+                <input type="email" require>
+            </div>
+            <div>
+                <label for="">Senha:</label>
+                <input type="password" require>
+            </div>
+            <button>Entrar</button>
+            <span>&copy;techSoluctions</span>
+        </form>
+        
+    </section>
+</body>
+
+</html>
